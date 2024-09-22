3c2ab8fd81b12f534a96aa80a0561b718ad9be2b
andre <andrew@gogoberidze.ru>
2024-09-22 16:32:07 +0300

    LED and Switch

---
+++ components/led.gd
@@ -0,0 +1,16 @@
+extends CircuitComponent
+class_name LED
+var sprite: Sprite2D
+func initialize(spec: ComponentSpecification)->void:
+	super.initialize(spec)
+	sprite = Sprite2D.new()
+	sprite.texture = test_texture
+	sprite.modulate = Color(0, 100, 0, 0.2)
+	add_child(sprite)
+
+func _process(delta: float)->void:
+	super._process(delta)
+	if pins[0].state == NetConstants.LEVEL.LEVEL_HIGH:
+		sprite.modulate = Color(0, 100, 0, 1)
+	else:
+		sprite.modulate = Color(0, 100, 0, 0.2)
