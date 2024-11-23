79a7ba64891b781e6f620a0bebc53074e0018821
andre <andrew@gogoberidze.ru>
2024-11-23 13:30:16 +0300

    Fixed small bugs and discrepencies

--- components/ic/flipflops/K1533TM2.gd
+++ components/ic/flipflops/K1533TM2.gd
@@ -16,12 +16,15 @@ func _ready() -> void:
 func _process_signal():
 	pin(7).set_low()
 	pin(14).set_high()
-	if(pin(1).low): # D1 RST
+	if(pin(1).low && pin(2).high): # D1 RST
 		pin(5).set_low()
 		pin(6).set_high()
-	elif pin(2).low: #TODO: Verify rst priority
+	elif (pin(2).low && pin(1).high): 
 		pin(5).set_high() # D1 SET
 		pin(6).set_low()
+	elif (pin(1).low && pin(2).low):
+		pin(5).set_high()
+		pin(6).set_high()
 	else:
 		if(previous_clock_1==NetConstants.LEVEL.LEVEL_LOW and pin(3).high): # Rising edge on C1
 			if(pin(2).low): # If input D1 is low
@@ -31,12 +34,15 @@ func _process_signal():
 				pin(5).set_high() # D1 SET
 				pin(6).set_low()
 	previous_clock_1 = pin(3).state
-	if(pin(13).low): # D2 RST
+	if (pin(13).low && pin(10).high): # D2 RST
 		pin(9).set_low()
 		pin(8).set_high()
-	elif pin(10).low: #TODO: Verify rst priority
+	elif (pin(10).low && pin(13).high):
 		pin(9).set_high() # D2 SET
 		pin(8).set_low()
+	elif (pin(10).low && pin(13).low):
+		pin(9).set_high()
+		pin(8).set_high()
 	else:
 		if(previous_clock_2==NetConstants.LEVEL.LEVEL_LOW and pin(11).high): # Rising edge on C2
 			if(pin(12).low): # If input D2 is low
