7ed5a0251f127c03f3b83cc00cf315c7095db349
andre <andrew6gog@gmail.com>
2024-11-29 10:08:02 +0300

    Added LE1, fixed LL1

--- components/ic/logic/K1533LL1.gd
+++ components/ic/logic/K1533LL1.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 func _process_signal():
 	pin(7).set_low()
 	pin(14).set_high()
-	pin(3).state = (not (pin(2).high || pin(1).high)) as int
-	pin(6).state = (not (pin(4).high || pin(5).high)) as int
-	pin(8).state = (not (pin(9).high || pin(10).high)) as int
-	pin(11).state = (not (pin(12).high || pin(13).high)) as int
+	pin(3).state = ( (pin(2).high || pin(1).high)) as int
+	pin(6).state = ( (pin(4).high || pin(5).high)) as int
+	pin(8).state = ( (pin(9).high || pin(10).high)) as int
+	pin(11).state = ( (pin(12).high || pin(13).high)) as int
