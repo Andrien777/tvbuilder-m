0dce9dc878ba7b41bf590b8ec015d6410869af6d
andre <andrew@gogoberidze.ru>
2024-12-19 19:43:49 +0300

    Changed graphic mode switch, changed grid panning, grid zoom is still terrible

--- ui/camera_2d.gd
+++ ui/camera_2d.gd
@@ -3,42 +3,36 @@ extends Camera2D
 var pressed_mmb = false
 var prev_pos
 var grid_rect 
+var delta_vec = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	grid_rect = get_node("../GridLayer/GridRect")
-	grid_rect.material.set_shader_parameter("position",Vector2(6, 5))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	grid_rect.material.set_shader_parameter("position",position*zoom + 0.1 * delta_vec)
 	if(GlobalSettings.disableGlobalInput):
 		return
 	if Input.is_action_just_pressed("ZoomUp"):
 		change_zoom(Vector2(0.1,0.1))
-		grid_rect.material.set_shader_parameter("scale", Vector2.ONE/zoom.x)
 	elif Input.is_action_just_pressed("ZoomDown"):
 		if zoom.x > 0.1 and zoom.y > 0.1:
 			change_zoom(Vector2(-0.1,-0.1))
-			grid_rect.material.set_shader_parameter("scale",  Vector2.ONE/zoom.x)
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("pan_up"):
 		position += Vector2.UP * 10
-		grid_rect.material.set_shader_parameter("position",grid_rect.get_material().get_shader_parameter("position") - Vector2.UP * 5 * zoom)
 	if Input.is_action_pressed("pan_down"):
 		position += Vector2.DOWN * 10
-		grid_rect.material.set_shader_parameter("position",grid_rect.get_material().get_shader_parameter("position") - Vector2.DOWN * 5 * zoom)
 	if Input.is_action_pressed("pan_left"):
 		position += Vector2.LEFT * 10
-		grid_rect.material.set_shader_parameter("position",grid_rect.get_material().get_shader_parameter("position") + Vector2.LEFT * 5 * zoom)
 	if Input.is_action_pressed("pan_right"):
 		position += Vector2.RIGHT * 10
-		grid_rect.material.set_shader_parameter("position",grid_rect.get_material().get_shader_parameter("position") + Vector2.RIGHT * 5 * zoom)
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		if pressed_mmb:
 			var delta_vec = Input.get_last_mouse_velocity() * delta / zoom
 			position -= delta_vec
-			grid_rect.material.set_shader_parameter("position",grid_rect.get_material().get_shader_parameter("position") - delta_vec * Vector2(zoom.x / 1150, zoom.y / 650))
 		prev_pos = get_global_mouse_position()
 	
 	pressed_mmb = Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT)
@@ -47,4 +41,8 @@ func change_zoom(delta: Vector2) -> void:
 	var mouse_pos := get_global_mouse_position()
 	zoom += delta
 	var new_mouse_pos := get_global_mouse_position()
+	grid_rect.material.set_shader_parameter("scale", grid_rect.get_material().get_shader_parameter("scale") + delta)
 	position += mouse_pos - new_mouse_pos
+	delta_vec = mouse_pos - new_mouse_pos
+	#grid_rect.material.set_shader_parameter("position",grid_rect.get_material().get_shader_parameter("position") + delta_vec * Vector2(-zoom.x/2,zoom.y/2))
+	
