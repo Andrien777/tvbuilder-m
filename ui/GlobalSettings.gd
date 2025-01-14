47a9dcd19be7fbcb77b3fd3b72c6d61f90846286
andre <andrew@gogoberidze.ru>
2025-01-14 13:41:18 +0300

    Added wire color selector, settings embedding in save files, save files versioning

--- ui/GlobalSettings.gd
+++ ui/GlobalSettings.gd
@@ -12,6 +12,10 @@ var showLastWire = false
 var highlightOutputPins = false
 
 var bg_color = Color(0.5, 0.504, 0.004)
+var wire_color = Color(1, 0, 0)
+var useDefaultWireColor = true
+
+var allowSettingsOverride = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -46,6 +50,12 @@ func try_load():
 				showLastWire = parsed["ShowLastWire"] as bool
 			if parsed.has("BgColor"):
 				bg_color = Color(parsed["BgColor"])
+			if parsed.has("WireColor"):
+				wire_color = Color(parsed["WireColor"])
+			if parsed.has("DefaultWireColor"):
+				useDefaultWireColor = parsed["DefaultWireColor"] as bool
+			if parsed.has("SettingsOverride"):
+				allowSettingsOverride = parsed["SettingsOverride"] as bool
 				
 
 func save():
@@ -61,5 +71,16 @@ func save():
 			json_object["GraphicsMode"] = "DefaultGraphicsMode"
 	json_object["ShowLastWire"] = showLastWire as int
 	json_object["BgColor"] = bg_color.to_html(false)
+	json_object["WireColor"] = wire_color.to_html(false)
+	json_object["DefaultWireColor"] = useDefaultWireColor as int
+	json_object["SettingsOverride"] = allowSettingsOverride as int
 	file.store_string(JSON.stringify(json_object, "\t"))
 	file.close()
+
+func get_object_to_save():
+	var json_object = {}
+	json_object["version"] = 1
+	json_object["BgColor"] = bg_color.to_html(false)
+	json_object["WireColor"] = wire_color.to_html(false)
+	json_object["DefaultWireColor"] = useDefaultWireColor as int
+	return json_object
---
+++ ui/wire_color_picker_button.gd
@@ -0,0 +1,29 @@
+extends ColorPickerButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	color = GlobalSettings.wire_color
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_color_changed(color: Color) -> void:
+	GlobalSettings.wire_color = color
+	GlobalSettings.useDefaultWireColor = false
+	for wire in WireManager.wires:
+		wire.change_color()
+
+
+func _on_wire_color_reset_button_pressed() -> void:
+	GlobalSettings.useDefaultWireColor = true
+	if GlobalSettings.CurrentGraphicsMode == LegacyGraphicsMode:
+		color = Color(1, 0, 0)
+	else:
+		color = Color(1, 1, 1)
+	GlobalSettings.wire_color = color
+	for wire in WireManager.wires:
+		wire.change_color()
