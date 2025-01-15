81c097e6640d5c7c68d20079eb026c76a43efce2
Plezha <plehanovtemka@gmail.com>
2025-01-15 16:07:01 +0300

    Logic Analyzer Tweaks

--- ui/show_logic_analyzer_button.gd
+++ ui/show_logic_analyzer_button.gd
@@ -3,5 +3,5 @@ extends Button
 @onready var logic_analyzer_window = get_node("/root/RootNode/LogicAnalyzerWindow")
 
 func _on_pressed() -> void:
-	logic_analyzer_window.visible = true
+	logic_analyzer_window.show()
 	logic_analyzer_window.grab_focus()
