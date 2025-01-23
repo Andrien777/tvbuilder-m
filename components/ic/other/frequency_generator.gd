98b6fd1665b9ba9cc410c9854d0f9c409d506464
andre <andrew@gogoberidze.ru>
2025-01-23 18:33:03 +0300

    Burst mode in generator. Also changed IR22 pins

--- components/ic/other/frequency_generator.gd
+++ components/ic/other/frequency_generator.gd
@@ -2,11 +2,15 @@ extends CircuitComponent
 class_name FrequencyGenerator
 var timer
 var text_line: LineEdit
+var imp_line: LineEdit
+var hz_label
+var imp_label
 var settings_popup
 var enable_button
 var turbo_button
 var turbo = false
 var enabled = 0
+var imp_counter = -1
 var popup_style
 var freq_label = Label.new()
 func _init():
@@ -25,7 +29,7 @@ func _ready() -> void:
 	popup_style.bg_color =  Color.DIM_GRAY
 	popup_style.bg_color.a = 0.9
 	popup_style.set_corner_radius_all(15)
-	settings_popup.size = Vector2(250,100)
+	settings_popup.size = Vector2(250,150)
 	settings_popup.add_theme_stylebox_override("panel", popup_style)
 	settings_popup.visible = false
 	
@@ -34,7 +38,22 @@ func _ready() -> void:
 	text_line.max_length = 4
 	text_line.text = "1"
 	text_line.text_changed.connect(on_text_update)
-	text_line.position = Vector2(20,60)
+	text_line.position = Vector2(20,100)
+	
+	hz_label = Label.new()
+	hz_label.text = "Гц"
+	hz_label.position = Vector2(90,105)
+	
+	imp_line = LineEdit.new()
+	imp_line.context_menu_enabled = false
+	imp_line.max_length = 4
+	imp_line.text = ""
+	imp_line.text_changed.connect(on_imp_text_update)
+	imp_line.position = Vector2(20,60)
+	
+	imp_label = Label.new()
+	imp_label.text = "импульсов"
+	imp_label.position = Vector2(90,65)
 	
 	enable_button = CheckButton.new()
 	enable_button.pressed.connect(on_enable_button_press)
@@ -54,9 +73,11 @@ func _ready() -> void:
 	settings_popup.add_child(text_line)
 	settings_popup.add_child(enable_button)
 	settings_popup.add_child(turbo_button)
+	settings_popup.add_child(imp_line)
+	settings_popup.add_child(imp_label)
 	add_child(settings_popup)
 	add_child(freq_label)
-	start_timer()
+	settings_popup.add_child(hz_label)
 	pin(1).set_low()
 	pin(2).set_high()
 
@@ -77,12 +98,19 @@ func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void
 
 func _process_signal():
 	if enabled and turbo:
-		if(pin(1).high):
+		if(pin(1).high) and imp_counter != 0:
 			pin(1).set_low()
 			pin(2).set_high()
+			if imp_counter > 0:
+				imp_counter -= 1
 		else:
 			pin(1).set_high()
 			pin(2).set_low()
+	if imp_counter == 0:
+		enabled = 0
+		enable_button.button_pressed = false
+		stop_timer()
+		imp_counter = int(imp_line.text)
 
 
 func stop_timer(): # might be useful in case graphics need to hook into timer events
@@ -90,10 +118,12 @@ func stop_timer(): # might be useful in case graphics need to hook into timer ev
 func start_timer():
 	timer.start()
 func on_timer_timeout():
-	if(enabled and not turbo):
+	if(enabled and not turbo and imp_counter != 0):
 		if(pin(1).high):
 			pin(1).set_low()
 			pin(2).set_high()
+			if imp_counter > 0:
+				imp_counter -= 1
 		else:
 			pin(1).set_high()
 			pin(2).set_low()
@@ -108,18 +138,36 @@ func on_text_update(new_text:String):
 			popup_style.bg_color =  Color.DIM_GRAY
 	else:
 		popup_style.bg_color =  Color.BROWN
+func on_imp_text_update(new_text:String):
+	if new_text.is_valid_int():
+		var imp = int(new_text)
+		if imp > 0:
+			imp_counter = imp
+		else:
+			imp_counter = -1
+			imp_line.text = ""
+	else:
+		imp_counter = -1
+		imp_line.text = ""
+	
+	
 func on_enable_button_press():
 	enabled = !enabled
 	if(!enabled):
 		pin(1).set_low()
+		pin(2).set_high()
+	else:
+		start_timer()
 
 func on_turbo_button_press():
 	turbo = !turbo
 	if(turbo):
 		text_line.visible = false
+		hz_label.visible = false
 		stop_timer()
 	else:
 		text_line.visible = true
+		hz_label.visible = true
 		start_timer()
 		
 func change_graphics_mode(mode):
