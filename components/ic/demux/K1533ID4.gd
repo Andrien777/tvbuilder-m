9d6255e18f6617f2ddd954648e0c89b19713ef16
andre <andrew@gogoberidze.ru>
2025-02-11 16:58:36 +0300

    More fixes

--- components/ic/demux/K1533ID4.gd
+++ components/ic/demux/K1533ID4.gd
@@ -5,7 +5,7 @@ class_name K1533ID4
 func _process_signal():
 	pin(8).set_low()
 	pin(16).set_high()
-	if pin(1).low: # Bottom half enabled
+	if pin(1).high and pin(2).low: # Bottom half enabled
 		if (pin(3).high_or_z and pin(13).high_or_z):
 			pin(7).set_high()
 			pin(6).set_high()
@@ -32,7 +32,7 @@ func _process_signal():
 		pin(5).set_high()
 		pin(4).set_high()
 
-	if pin(15).low: # Upper half enabled
+	if pin(15).low and pin(14).low: # Upper half enabled
 		if (pin(3).high_or_z and pin(13).high_or_z):
 			pin(9).set_high()
 			pin(10).set_high()
