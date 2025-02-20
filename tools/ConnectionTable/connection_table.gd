24f591c9537812ae714d3a29f251d98a03427c6c
nikitkagorkov@gmail.com <gerosp>
2025-02-20 17:46:53 +0300

    Implemented pin viewer in connection table. Fixed bug where some switches in settings lost their signals

--- tools/ConnectionTable/connection_table.gd
+++ tools/ConnectionTable/connection_table.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
-var grid_container
+
 var ic
 var connections = {}
 var continuous_update = false
+@onready var grid_container = $Window/TabContainer/VBoxContainer/ScrollContainer/GridContainer
+@onready var pin_desc_container = $Window/TabContainer/ScrollContainer/PinDescriptionContainer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	grid_container = $Window/VBoxContainer/ScrollContainer/GridContainer
 	$Window.close_requested.connect(close)
+	$Window/TabContainer.set_tab_title(0,"Таблица соединений")
+	$Window/TabContainer.set_tab_title(1,"Таблица ножек")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,6 +18,8 @@ func _process(delta: float) -> void:
 		get_connections(ic)
 		#display_connections()
 func display_connections():
+	set_pins_description_table(ic)
+	print(GlobalSettings.ShowSignalsInConnectionTable)
 	if GlobalSettings.ShowSignalsInConnectionTable:
 		grid_container.columns = 4
 		grid_container.get_node("SignalStateHeader").visible = true
@@ -53,6 +58,46 @@ func get_connections(object):
 					connections[wire.second_object.index].append({"id": wire.first_object.parent.id,"index": wire.first_object.index})
 				else:
 					connections[wire.second_object.index] = [{"id": wire.first_object.parent.id,"index": wire.first_object.index}]
+
+func set_pins_description_table(object):
+	if is_instance_valid(object):
+		var index_label = Label.new()
+		var name_label = Label.new()
+		var description_label = Label.new()
+		var direction_label = Label.new()
+		index_label.text = "Номер"
+		name_label.text = "Имя"
+		description_label.text = "Описание"
+		direction_label.text = "Направление"
+		pin_desc_container.add_child(index_label)
+		pin_desc_container.add_child(VSeparator.new())
+		pin_desc_container.add_child(name_label)
+		pin_desc_container.add_child(VSeparator.new())
+		pin_desc_container.add_child(description_label)
+		pin_desc_container.add_child(VSeparator.new())
+		pin_desc_container.add_child(direction_label)
+		for pin in object.pins:
+			index_label = Label.new()
+			name_label = Label.new()
+			description_label = Label.new()
+			direction_label = Label.new()
+			index_label.text = str(pin.index)
+			name_label.text = pin.readable_name
+			description_label.text = pin.description
+			match pin.direction:
+				NetConstants.DIRECTION.DIRECTION_INPUT:
+					direction_label.text = "Ввод"
+				NetConstants.DIRECTION.DIRECTION_OUTPUT:
+					direction_label.text = "Вывод"
+				NetConstants.DIRECTION.DIRECTION_INPUT_OUTPUT:
+					direction_label.text = "Двунаправленный"
+			pin_desc_container.add_child(index_label)
+			pin_desc_container.add_child(VSeparator.new())
+			pin_desc_container.add_child(name_label)
+			pin_desc_container.add_child(VSeparator.new())
+			pin_desc_container.add_child(description_label)
+			pin_desc_container.add_child(VSeparator.new())
+			pin_desc_container.add_child(direction_label)
 
 func close():
 	self.queue_free()
