1e518e039644eb67ebbbade8115fb641fdc1d99c
andre <andrew@gogoberidze.ru>
2025-04-10 20:31:48 +0300

    Reverted IR22

--- components/ic/registers/K1531IR22.gd
+++ components/ic/registers/K1531IR22.gd
@@ -2,18 +2,13 @@ extends CircuitComponent
 class_name K1531IR22
 
 var state: Array
-var temp_state: Array
-var temp_state_2: Array
 var inputs: Array
 var outputs: Array
-var counter = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	inputs = [3, 4, 7, 8, 13, 14, 17, 18]
 	outputs = [2, 5, 6, 9, 12, 15, 16, 19]
 	state = [0, 0, 0, 0, 0, 0, 0, 0]
-	temp_state_2 = [0, 0, 0, 0, 0, 0, 0, 0]
-	temp_state = [0, 0, 0, 0, 0, 0, 0, 0]
 	
 
 func save_state():
@@ -32,15 +27,7 @@ func _process_signal():
 	pin(20).set_high()
 	if (pin(1).low):
 		if pin(11).high:
-			for i in range(inputs.size()):
-				temp_state[i] = pin(inputs[i]).high_or_z as int
-			if temp_state == temp_state_2:
-				counter += 1
-			else:
-				counter = 0
-				temp_state_2 = temp_state.duplicate(true)
-			if counter == 5:
-				save_state()
+			save_state()
 		load_state()
 	else:
 		for i in range(outputs.size()):
